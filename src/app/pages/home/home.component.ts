b53125b34db6bd5c57d040a57128f7712ac54620
slimRelief <slim.reliefapps@gmail.com>
2020-08-08 18:53:02 +0100

    Add second section

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  iconClicked = false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  changeText() {
+    if (this.iconClicked) {
+      document.getElementById('changing-text').innerHTML = 'de monitoring (indicateurs et données), accompagné des baselines, cibles et collectes passées pour chacun d`eux.';
+      document.getElementById('arrow-icon').innerHTML = 'keyboard_arrow_up';
+    }
+    else {
+      document.getElementById('changing-text').innerHTML = 'Avec la mise à disposition des cadres logiques des autres projets et du plan..';
+      document.getElementById('arrow-icon').innerHTML = 'keyboard_arrow_down';
+    }
+    this.iconClicked = !this.iconClicked;
+  }
 }
