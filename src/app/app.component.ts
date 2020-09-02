6325f39f28b673a5ed3abfdbc7e318f997841258
antoineHurard <antoine.reliefapps@gmail.com>
2020-09-02 09:08:08 +0200

    Add all icons to registry

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,12 +19,92 @@ export class AppComponent {
       this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/add-folder.svg')
     );
     this.matIconRegistry.addSvgIcon(
+      'box-white',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/box-white.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'bulb',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/bulb.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'check',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/check.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'chevron',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/chevron.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'clipboard',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/clipboard.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'database',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/database.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'folder',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/folder.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'format-shapes',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/format-shapes.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'gauge',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/gauge.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'grid',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/grid.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'history',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/history.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'home',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/home.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'location',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/location.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'message',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/message.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'organigram',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/organigram.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'parameters',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/parameters.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'people',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/people.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
       'person',
       this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/person.svg')
     );
     this.matIconRegistry.addSvgIcon(
+      'pie-chart',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/pie-chart.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'quality',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/quality.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
       'search',
       this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/search.svg')
+    );
+    this.matIconRegistry.addSvgIcon(
+      'star',
+      this.domSanitizer.bypassSecurityTrustResourceUrl('../assets/svg/star.svg')
     );
   }
 }
