b9f99eb1b00e2f9ea47ea798b41b44603d12fea1
antoineHurard <antoine.reliefapps@gmail.com>
2020-09-04 09:36:28 +0200

    Add translate basics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { SearchbarModule } from './components/searchbar/searchbar.module';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { MatIconModule } from '@angular/material/icon';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { ProjectModule } from './components/project/project.module';
@@ -23,6 +23,8 @@ import { IndicatorModule } from './components/indicator/indicator.module';
 import { HomeIllustrationModule } from './components/home-illustration/home-illustration.module';
 import { HomeCardModule } from './components/home-card/home-card.module';
 import { LoginComponent } from './components/login/login.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 @NgModule({
   declarations: [
@@ -34,6 +36,13 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: httpTranslateLoader,
+        deps: [HttpClient]
+      }
+    }),
     BrowserModule,
     AppRoutingModule,
     HeaderModule,
@@ -66,3 +75,7 @@ import { LoginComponent } from './components/login/login.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+export function httpTranslateLoader(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
