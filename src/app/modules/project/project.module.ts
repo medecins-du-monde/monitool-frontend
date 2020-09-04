4886015a0a54a8c82cf79cd2445db091eb41c287
antoineHurard <antoine.reliefapps@gmail.com>
2020-09-04 10:41:35 +0200

    Add translation project module

--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -4,11 +4,13 @@ import { ProjectComponent } from './project.component';
 import { ProjectRoutingModule } from './project-routing.module';
 import { SidenavModule } from 'src/app/components/sidenav/sidenav.module';
 import { SvgIconsModule } from '@ngneat/svg-icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [ProjectComponent],
   imports: [
     CommonModule,
+    TranslateModule,
     ProjectRoutingModule,
     SidenavModule,
     SvgIconsModule
