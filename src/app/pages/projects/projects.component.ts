c19dec61a9f0da8acdc8d9f615a9dce4fc845f9b
Antoine Hurard <antoinehurard@macbook-pro-de-antoine.home>
2020-09-07 12:06:54 +0200

    Recreate all filters

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -9,107 +9,70 @@ import { projectsList } from 'src/app/constants/projects';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-  // btn1Clicked = true;
-  // btn2Clicked = false;
-  // btn3Clicked = false;
-  // countries: { option: string, value: string }[] = [
-  //   {
-  //     option: 'Tous les pays',
-  //     value: 'tous'
-  //   }
-  // ];
-  countries = ['Country 1', 'Country 2', 'Country3 '];
+
+  searchText: string;
+  countries = ['Burkina Faso', 'Italie', 'Inde'];
   themes = ['Theme 1', 'Theme 2'];
+  statuses = [
+    {
+      text: 'OngoingPlural',
+      value: 'Ongoing'
+    },
+    {
+      text: 'FinishedPlural',
+      value: 'Finished'
+    },
+    {
+      text: 'DeletedPlural',
+      value: 'Deleted'
+    }
+  ];
+
   filtersForm: FormGroup;
-  // selectedCountry: string;
   projects: Project[];
-  // currentProjectList: Project[];
-  // search = '';
-  // searchedProjects: Project[];
-  // filteredCountryProjects: Project[];
 
   constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
     this.filtersForm = this.fb.group({
-      countries: [[], Validators.required],
-      themes: [[], Validators.required]
+      countries: [[]],
+      themes: [[]],
+      statuses: [['Ongoing']]
     });
-    // this.selectedCountry = this.countries[0].value;
-    this.projects = projectsList.filter(project => project.active === true && new Date(Date.now()) < project.end);
-    // this.currentProjectList = this.projects;
-    // this.searchedProjects = this.projects;
-    // this.initiateSelectMenu();
+    this.projects = projectsList.filter(project => project.active === true);
+    this.filtersForm.controls.countries.valueChanges.subscribe(e => {
+      this.onSelectCountries(e);
+    });
+    this.filtersForm.controls.statuses.valueChanges.subscribe(e => {
+      this.onSelectStatuses(e);
+    });
   }
 
   onSearch(e: any): void {
+    this.searchText = e;
     this.projects = projectsList.filter(project => project.name.toLowerCase().includes(e.toLowerCase()));
   }
 
-  // onBtnClick(id) {
-  //   if (id === 'btn-1') {
-  //     this.btn1Clicked ? this.currentProjectList
-  // = this.currentProjectList.filter(p => p.active ===
-  // false || new Date(Date.now()) > p.end)
-  //       : this.currentProjectList.push(...projectsList.filter
-  // (project => project.active === true &&
-  // new Date(Date.now()) < project.end));
-  //     this.btn1Clicked = !this.btn1Clicked;
-  //   }
-  //   else if (id === 'btn-2') {
-  //     this.btn2Clicked ? this.currentProjectList =
-  // this.currentProjectList.filter(p => p.active === false ||
-  // new Date(Date.now()) < p.end)
-  //       : this.currentProjectList.
-  // push(...projectsList.filter(project =>
-  // project.active === true && new Date(Date.now()) > project.end));
-  //     this.btn2Clicked = !this.btn2Clicked;
-  //   }
-  //   else if (id === 'btn-3') {
-  //     this.btn3Clicked ? this.currentProjectList = this.currentProjectList.filter(p => p.active === true)
-  //       : this.currentProjectList.push(...projectsList.filter(project => project.active === false));
-  //     this.btn3Clicked = !this.btn3Clicked;
-  //   }
-  //   this.projects = this.currentProjectList;
-  //   this.onSelectMenuChanged();
-  //   if (this.search !== '') {
-  //     this.onSearchbarChanged();
-  //   }
-  // }
+  private onSelectCountries(countries: string[]): void {
+    if (countries.length > 0) {
+      this.projects = projectsList.filter(project => countries.includes(project.country));
+    } else {
+      this.projects = projectsList;
+    }
+  }
 
-  // onSearchbarChanged() {
-  //   this.projects = this.currentProjectList.filter(project => project.name.startsWith(this.search));
-  //   this.searchedProjects = this.projects;
-  //   if (this.selectedCountry !== 'tous') {
-  //     this.projects = this.filteredCountryProjects.filter(project => project.name.startsWith(this.search));
-  //   }
-  // }
-
-  // onSelectMenuChanged() {
-  //   if (this.selectedCountry !== 'tous') {
-  //     this.projects = this.currentProjectList.filter(project => project.country === this.selectedCountry);
-  //     this.filteredCountryProjects = this.projects;
-  //   }
-  //   else {
-  //     this.projects = this.currentProjectList;
-  //   }
-  //   if (this.search !== '') {
-  //     if (this.selectedCountry !== 'tous') {
-  //       this.projects = this.searchedProjects.filter(project => project.country === this.selectedCountry);
-  //     }
-  //     else {
-  //       this.projects = this.searchedProjects;
-  //     }
-  //   }
-  // }
-
-  // initiateSelectMenu() {
-  //   projectsList.forEach(project => {
-  //     if (this.countries.findIndex(x => x.option === project.country) === -1) {
-  //       this.countries.push({ option: project.country, value: project.country });
-  //     }
-  //   });
-  //   this.countries[0].option += ' (' + (this.countries.length - 1) + ')';
-  // }
+  private onSelectStatuses(statuses: string[]): void {
+    let filteredProjects = [];
+    if (statuses.includes('Ongoing')) {
+      filteredProjects = filteredProjects.concat(projectsList.filter(project => project.active));
+    }
+    if (statuses.includes('Finished')) {
+      filteredProjects = filteredProjects.concat(projectsList.filter(project => !project.active && project.end < new Date()));
+    }
+    if (statuses.includes('Deleted')) {
+      filteredProjects = filteredProjects.concat(projectsList.filter(project => !project.active && project.end > new Date()));
+    }
+    this.projects = filteredProjects;
+  }
 }
