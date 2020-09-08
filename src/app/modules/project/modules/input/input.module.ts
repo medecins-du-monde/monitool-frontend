8ce0c14e3f338a94a19b5ac716ff480d6c8236e4
Pedro Diogo <pedro.reliefapps@gmail.com>
2020-09-08 10:14:23 -0300

    added info sections and table

--- src/app/modules/project/modules/input/input.module.ts
+++ src/app/modules/project/modules/input/input.module.ts
@@ -2,14 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './pages/home/home.component';
 import { InputRoutingModule } from './input-routing.module';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { TranslateModule } from '@ngx-translate/core';
+import { ProgressBarModule } from 'src/app/components/progress-bar/progress-bar.module';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    InputRoutingModule
+    TranslateModule,
+    InputRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    ProgressBarModule
   ]
 })
 export class InputModule { }
--- src/app/modules/project/modules/input/pages/home/home.component.ts
+++ src/app/modules/project/modules/input/pages/home/home.component.ts
@@ -1,15 +1,83 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Task {
+  taskText1: string;
+  buttonText1: string;
+  buttonIcon1: string;
+  status: number;
+  routerLink1: string;
+}
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  displayedColumns: string[] = ['task', 'status'];
+  dataSource: Task[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.dataSource = [
+      {
+        taskText1: 'Remplir les 119 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Extraction du SNIS (CSP)',
+        status: 100,
+        routerLink1: `.`,
+      },
+      {
+        taskText1: 'Remplir les 119 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Registre pharmacie (CSP)',
+        status: 30,
+        routerLink1: `.`,
+      },
+      {
+        taskText1: 'Remplir les 42 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Rapport de supervision trimestriel (CSP)',
+        status: 45,
+        routerLink1: `.`,
+      },
+      {
+        taskText1: 'Remplir les 144 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Registre des VLG (CSP & Unité mobile)',
+        status: 0,
+        routerLink1: `.`,
+      },
+      {
+        taskText1: 'Remplir les 36 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Extraction du SNIS (Hôpital de référence)',
+        status: 0,
+        routerLink1: `.`,
+      },
+      {
+        taskText1: 'Remplir les 36 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Registre des VLG (Hôpital de référence)',
+        status: 0,
+        routerLink1: `.`,
+      },
+      {
+        taskText1: 'Remplir les 126 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Recensement',
+        status: 0,
+        routerLink1: `.`,
+      },
+      {
+        taskText1: 'Remplir les 6 saisies de ',
+        buttonIcon1: 'edit',
+        buttonText1: 'Coordination',
+        status: 0,
+        routerLink1: `.`,
+      },
+    ];
   }
 
 }
