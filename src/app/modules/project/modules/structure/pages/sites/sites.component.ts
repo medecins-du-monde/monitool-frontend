d0b12a917501d8ab5ec6f5547a0bbcb8fd62411a
Pedro Diogo <pedro.reliefapps@gmail.com>
2020-10-06 08:29:42 -0300

    merge front-end for sites page from other branch

--- src/app/modules/project/modules/structure/pages/sites/sites.component.ts
+++ src/app/modules/project/modules/structure/pages/sites/sites.component.ts
@@ -1,4 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface CollectionSite {
+  name: string;
+  startDate: Date;
+  endDate: Date;
+}
+
+export interface Group {
+  name: string;
+  sites: CollectionSite[];
+}
 
 @Component({
   selector: 'app-sites',
@@ -6,10 +19,72 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sites.component.scss']
 })
 export class SitesComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  sitesDisplayedColumns: string[] = ['position', 'name', 'startDate', 'endDate', 'delete'];
+
+  groupsDisplayedColumns: string[] = ['position', 'name', 'sites', 'delete'];
+
+  constructor(private fb: FormBuilder) { }
+
 
   ngOnInit(): void {
+    const sites = [
+      {
+        name: 'Site 1',
+        startDate: new Date(),
+        endDate: new Date()
+      },
+      {
+        name: 'Site 2',
+        startDate: new Date(),
+        endDate: new Date()
+      }
+    ];
+
+    this.form = this.fb.group({
+      sites: this.fb.array(sites.map(x => this.createSite(x))),
+      groups: this.fb.array([this.createGroup()])
+    });
   }
+
+  get sites() {
+    return this.form.get('sites') as FormArray;
+  }
+
+  get groups() {
+    return this.form.get('groups') as FormArray;
+  }
+  public addSite() {
+    this.sites.push(this.createSite());
+  }
+
+  public removeSite(index: number) {
+    this.sites.removeAt(index);
+  }
+
+  public addGroup() {
+    this.groups.push(this.createGroup());
+  }
+
+  public removeGroup(index: number) {
+    this.groups.removeAt(index);
+  }
+
+  private createSite(site?: CollectionSite): FormGroup {
+    return this.fb.group({
+      name: [site ? site.name : '', Validators.required],
+      startDate: [site ? site.startDate : new Date(), Validators.required],
+      endDate: [site ? site.endDate : new Date(), Validators.required],
+    });
+  }
+
+  private createGroup(): FormGroup {
+    return this.fb.group({
+      name: ['', Validators.required],
+      sites: ['']
+    });
+  }
+
 
 }
--- src/app/modules/project/modules/structure/pages/sites/sites.module.ts
+++ src/app/modules/project/modules/structure/pages/sites/sites.module.ts
@@ -3,12 +3,40 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { SitesRoutingModule } from './sites-routing.module';
 import { SitesComponent } from './sites.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTableModule } from '@angular/material/table';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkTableModule } from '@angular/cdk/table';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+
 
 @NgModule({
     declarations: [SitesComponent],
     imports: [
         CommonModule,
         TranslateModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatButtonModule,
+        MatIconModule,
+        MatExpansionModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatNativeDateModule,
+        MatTableModule,
+        DragDropModule,
+        CdkTableModule,
         SitesRoutingModule
     ]
 })
