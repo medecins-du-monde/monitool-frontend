8f8ef1454ac6cc1d8a9415e3969fa2e7798f91fe
Julien Foschia <julien.reliefapps@gmail.com>
2020-10-28 18:38:11 +0100

    fix of the type problem inside the edit

--- src/app/models/project-indicator.model.ts
+++ src/app/models/project-indicator.model.ts
@@ -4,6 +4,7 @@ import { Deserializable } from './deserializable.model';
 export const PERCENTAGE_FORMULA = '100 * numerator / denominator';
 export const PERMILLE_FORMULA = '1000 * numerator / denominator';
 export const COPY_FORMULA = 'copied_value';
+export const UNAVAIlABLE = 'unavailable';
 
 export class ProjectIndicator implements Deserializable {
 
@@ -15,7 +16,7 @@ export class ProjectIndicator implements Deserializable {
         formula: null,
         parameters: {}
     };
-    type = 'unavailable';
+    type = UNAVAIlABLE;
 
     constructor(input?: any) {
         this.deserialize(input);
@@ -25,23 +26,22 @@ export class ProjectIndicator implements Deserializable {
       Object.assign(this, input);
       this.colorize = this.colorize ? this.colorize : true;
       if (input && input.computation) {
-            if (!isNaN(input.computation.formula)) {
-                this.type = 'fixed';
-                this.computation.formula = input.computation.formula;
-                this.computation.parameters = {};
-            } else if (input.formula === COPY_FORMULA) {
+        this.type = input.type ? input.type : this.type;
+        this.computation.formula = input.computation.formula;
+        this.computation.parameters = {};
+        if (input.computation.formula === COPY_FORMULA) {
                 this.type = 'copy';
                 this.computation.formula = COPY_FORMULA;
                 this.computation.parameters = input.computation.parameters;
-            } else if (input.formula === PERCENTAGE_FORMULA) {
+            } else if (input.computation.formula === PERCENTAGE_FORMULA) {
                 this.type = 'percentage';
                 this.computation.formula = PERCENTAGE_FORMULA;
                 this.computation.parameters = input.computation.parameters;
-            } else if (input.formula === PERMILLE_FORMULA) {
+            } else if (input.computation.formula === PERMILLE_FORMULA) {
                 this.type = 'permille';
                 this.computation.formula = PERMILLE_FORMULA;
                 this.computation.parameters = input.computation.parameters;
-            } else {
+            } else if (input.computation.formula && input.computation.parameters === {}){
                 this.type = 'formula';
                 this.computation.formula = input.computation.formula;
                 this.computation.parameters = input.computation.parameters;
