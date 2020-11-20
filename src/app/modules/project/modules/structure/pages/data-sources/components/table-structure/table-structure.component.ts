24bda87e419df4df210d3b27db84527bd85ee11e
Theresa Tobollik <theresa.joana.tobollik@uni-weimar.de>
2020-11-20 15:45:31 +0100

    Reorder table structure based on ID

--- src/app/modules/project/modules/structure/pages/data-sources/components/table-structure/table-structure.component.ts
+++ src/app/modules/project/modules/structure/pages/data-sources/components/table-structure/table-structure.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Project } from 'src/app/models/project.model';
 import { ProjectService } from 'src/app/services/project.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-table-structure',
@@ -36,14 +37,12 @@ export class TableStructureComponent implements OnInit {
     return false;
   }
 
-
   reorderPartitions(name) {
-      console.log(this.partitions);
-      const indexNew = this.partitions.findIndex(element => element.name === this.chosenValue);
-      const indexOld = this.partitions.findIndex(element => element.name === name);
-      [this.partitions[indexOld], this.partitions[indexNew]] =[this.partitions[indexNew], this.partitions[indexOld]];
-      console.log(this.partitions)
+      const indexNew = this.partitions.findIndex(element => element.id === this.chosenValue);
+      const indexOld = this.partitions.findIndex(element => element.id === name);
+      var old = this.partitions[indexOld];
+      this.partitions[indexOld] = this.partitions[indexNew]
+      this.partitions[indexNew] = old;
   }
-
 
 }
