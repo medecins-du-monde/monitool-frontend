a06aeecf4810e6dcc2ed08fafe18ba85578eb4cc
Theresa Tobollik <theresa.joana.tobollik@uni-weimar.de>
2020-11-21 17:36:08 +0100

    Dynamic colspan for radio buttons

--- src/app/modules/project/modules/structure/pages/data-sources/components/table-structure/table-structure.component.ts
+++ src/app/modules/project/modules/structure/pages/data-sources/components/table-structure/table-structure.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { Project } from 'src/app/models/project.model';
 import { FormControl } from '@angular/forms';
 import { ProjectService } from 'src/app/services/project.service';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-table-structure',
@@ -39,11 +40,14 @@ export class TableStructureComponent implements OnInit {
   }
 
   reorderPartitions(event, name) {
-      const indexNew = this.partitions.findIndex(element => element.id === event.value);
-      const indexOld = this.partitions.findIndex(element => element.id === name);
+      console.log(event)
+      console.log(name)
+      const indexNew = this.partitions.findIndex(element => element.name === event.value);
+      const indexOld = this.partitions.findIndex(element => element.name === name);
       var old = this.partitions[indexOld];
       this.partitions[indexOld] = this.partitions[indexNew]
       this.partitions[indexNew] = old;
+      console.log(this.partitions)
   }
 
   getNumber(number) {
@@ -53,6 +57,7 @@ export class TableStructureComponent implements OnInit {
   }
 
   toNumber(i) {
+    console.log(i)
     return parseInt(i);
   }
 
