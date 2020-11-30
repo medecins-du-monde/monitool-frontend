ee33761a2983b450deeb7b4162126c5d4bad904d
Theresa Tobollik <theresa.joana.tobollik@uni-weimar.de>
2020-11-30 11:05:22 +0100

    Add basic line chart

--- src/app/modules/project/modules/reporting/pages/general/chart/chart.component.ts
+++ src/app/modules/project/modules/reporting/pages/general/chart/chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart } from 'node_modules/chart.js';
 
 @Component({
   selector: 'app-chart',
@@ -10,6 +11,16 @@ export class ChartComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    var myChart = new Chart("MyChart", {
+      type: 'line',
+      data: {
+          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+          datasets: [{
+              label: '# of Votes',
+              data: [12, 19, 3, 5, 2, 3],
+          }]
+      }
+  });
   }
 
 }
