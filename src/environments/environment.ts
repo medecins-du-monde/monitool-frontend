15d36ee1e35280e8cbe686d1d1d50d8f7756a882
Julien Foschia <julien.reliefapps@gmail.com>
2020-12-16 16:49:30 +0100

    finilization of the method for the mdm account connection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,7 @@
 export const environment = {
   production: false,
-  API_URL: '/api'
+  API_URL: '/api',
+  clientId: 'd62083d8-fdc0-4a6a-8618-652380eebdb9',
+  authority: 'https://login.microsoftonline.com/fbacd48d-ccf4-480d-baf0-31048368055f',
+  redirectUrl: 'http://localhost:4200',
 };
