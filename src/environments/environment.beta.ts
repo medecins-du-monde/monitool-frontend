2cff42f61b0bdf9c24f8378867fad71d5094086f
Julien Foschia <julien.reliefapps@gmail.com>
2020-12-17 15:32:47 +0100

    refactoring and adding beta configuration

---
+++ src/environments/environment.beta.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  API_URL: '/api',
+  clientId: 'ec380c9b-e98c-4de9-a311-9fa2495e6daf',
+  authority: 'https://login.microsoftonline.com/3c3a3c6a-8d0a-446a-ae82-cee5b605904e',
+  redirectUrl: 'https://monitool.test.humanitarian.tech/api/authentication/login-callback',
+  postLogoutRedirectUri: 'https://monitool.test.humanitarian.tech/login'
+};
