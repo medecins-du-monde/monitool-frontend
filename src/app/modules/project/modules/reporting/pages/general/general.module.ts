e14eeae0653ca2b74afbc7413d8555fa69fcd0cc
Julien Foschia <julien.reliefapps@gmail.com>
2021-01-04 12:52:45 +0100

    other reorganization of the components

--- src/app/modules/project/modules/reporting/pages/general/general.module.ts
+++ src/app/modules/project/modules/reporting/pages/general/general.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
 import { GeneralRoutingModule } from './general-routing.module';
 import { GeneralComponent } from './general.component';
-import { ReportTableModule } from 'src/app/components/shared/report/report-table/report-table.module';
-import { ChartModule} from 'src/app/components/shared/chart/chart.module';
+import { ReportTableModule } from 'src/app/components/report/report-table/report-table.module';
+import { ChartModule} from 'src/app/components/chart/chart.module';
 import { ObjectGroupingModule} from './objectGrouping/object-grouping.module';
 import { FilterModule} from './filter/filter.module';
 import { DataTableModule } from './dataTable/data-table.module';
-import { ReportingTableModule } from 'src/app/components/shared/report/reporting-table/reporting-table.module';
+import { ReportingTableModule } from 'src/app/components/report/reporting-table/reporting-table.module';
 
 
 @NgModule({
