bd851996e7cddb01ca09cf8bdb4839ab0e92aa09
Dhia69i <dhia.hamdi@esprit.tn>
2021-01-26 12:49:09 +0100

    cleaner push

--- src/app/pages/projects/components/project/project.component.ts
+++ src/app/pages/projects/components/project/project.component.ts
@@ -27,7 +27,7 @@ export class ProjectComponent implements OnInit {
   projectOwner: boolean;
   lastEntry: string;
 
-  get currentLang(): string{
+  get currentLang(): string {
     return this.translateService.currentLang ? this.translateService.currentLang : this.translateService.defaultLang;
   }
 
@@ -37,30 +37,15 @@ export class ProjectComponent implements OnInit {
     private authService: AuthService,
     private router: Router,
     private dialog: MatDialog,
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-   
+
     this.authService.currentUser.subscribe((user: User) => {
       this.currentUser = new User(user);
       this.projectOwner = (this.project.users.filter(projectUser => projectUser.id === this.currentUser.id).length > 0);
     })
-    //   this.projectService.list().then((projects: Project[]) => {
-    //   projects.map(w => {
-    //     if(w.status ==='Ongoing'){
-    //     document.getElementById('stat').style.backgroundColor = 'green';
-    //     }
-    //     else if(w.status ==='Finished'){
-    //       document.getElementById('stat').style.backgroundColor = 'white';
-    //       }
-    //       else if(w.status ==='Deleted'){
-    //         document.getElementById('stat').style.backgroundColor = 'red';
-    //         }
- 
-    // })
-    // });
-
- }
+  }
 
   async onOpen(): Promise<void> {
     this.projectService.get(this.project.id).then(() => {
@@ -68,19 +53,19 @@ export class ProjectComponent implements OnInit {
     });
   }
 
-  onDelete(): void{
+  onDelete(): void {
     this.delete.emit(this.project);
   }
 
-  onRestore(): void{
+  onRestore(): void {
     this.restore.emit(this.project);
   }
 
-  onClone(): void{
+  onClone(): void {
     const dialogRef = this.dialog.open(CloneProjectModalComponent);
 
     dialogRef.afterClosed().subscribe(res => {
-      if (res){
+      if (res) {
         this.clone.emit(this.project);
       }
     });
@@ -91,7 +76,7 @@ export class ProjectComponent implements OnInit {
       if (this.projectOwner) {
         return 'person';
       }
-      else if (localStorage.getItem('user::' + this.currentUser.id + 'favorite' + this.project.id)){
+      else if (localStorage.getItem('user::' + this.currentUser.id + 'favorite' + this.project.id)) {
         return 'star';
       } else {
         return 'star_border';
