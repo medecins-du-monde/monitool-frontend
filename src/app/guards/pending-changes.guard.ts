aea87d485b384197277cc6b977551b8435eaa2b7
Pedro Diogo <pedro.reliefapps@gmail.com>
2021-01-27 17:25:52 -0300

    add translation to the confirm message

--- src/app/guards/pending-changes.guard.ts
+++ src/app/guards/pending-changes.guard.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CanDeactivate } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
+import { MultiLanguage } from '../models/multi-language.model';
 import { ProjectService } from '../services/project.service';
 
 export interface ComponentCanDeactivate {
@@ -11,8 +13,18 @@ export interface ComponentCanDeactivate {
   providedIn: 'root'
 })
 export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate> {
+  
+  constructor(private projectService: ProjectService, private translateService: TranslateService){}
+  
+  get currentLang(): string {
+    return this.translateService.currentLang ? this.translateService.currentLang : this.translateService.defaultLang;
+  }
 
-  constructor(private projectService: ProjectService){}
+  warnMessage: MultiLanguage = new MultiLanguage({
+    en: 'You made changes. Click OK to confirm that you want to leave without saving.',
+    fr: 'Vous avez realisé des changements. Êtes-vous sûr de vouloir quitter sans sauvegarder?',
+    es: 'Ha realizado cambios. ¿Esta seguro de querer cambiar de página sin guardar?'
+  });
 
   // should return true when the page is allowed to close itself and false in the other case
   canDeactivate(component: ComponentCanDeactivate): boolean | Observable<boolean> {
@@ -20,12 +32,12 @@ export class PendingChangesGuard implements CanDeactivate<ComponentCanDeactivate
     if (component !== null){
       return component.canDeactivate() ?
         true : 
-        confirm('You made changes. Click OK to confirm that you want to leave without saving.');
+        confirm(this.warnMessage[this.currentLang]);
     }
 
     // if we have pending changes in the project we ask the user if he really wants to close the page
     if (this.projectService.hasPendingChanges) {
-      if (confirm('You made changes. Click OK to confirm that you want to leave without saving.')){
+      if (confirm(this.warnMessage[this.currentLang])){
         // if the user confirm, we discard the changes
         this.projectService.discardPendingChanges();
         return true;
