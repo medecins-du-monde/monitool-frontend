1a7ca0adde30cdf8cc99b6b2d5a17c7ef1f9c25e
Pedro Diogo <pedro.reliefapps@gmail.com>
2021-02-07 15:33:36 -0300

    Hide 'indicators' and 'settings' options for partner users

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,6 +3,7 @@ import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
 import { DateService} from 'src/app/services/date.service';
+import { User } from 'src/app/models/classes/user.model';
 
 @Component({
   selector: 'app-header',
@@ -12,30 +13,34 @@ import { DateService} from 'src/app/services/date.service';
 export class HeaderComponent implements OnInit {
 
   public isMobile: boolean;
-
+  private user: User;
+  
   constructor(
     private translateService: TranslateService,
     private dateService: DateService,
     private authService: AuthService,
-    private route: Router
+    private route: Router,
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void{
     window.onresize = () => this.isMobile = window.innerWidth < 600;
     this.dateService.setCurrentLang(localStorage.getItem('language'));
+    this.authService.currentUser.subscribe((user: User) => {
+      this.user = user;
+    })
   }
 
-  getLangs() {
+  getLangs(): string[]{
     return this.translateService.getLangs();
   }
 
-  switchLang(lang: string) {
+  switchLang(lang: string): void{
     this.translateService.use(lang);
     localStorage.setItem('language', lang);
     this.dateService.setCurrentLang(lang);
   }
 
-  disconnect(){
+  disconnect(): void{
     this.authService.logOut()
       .then( response => {
         if (response){
