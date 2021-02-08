9980e3c8269a52951ce4ab612df1b27c19e3f134
ziaulhaq41 <ziaul@oortcloud.tech>
2021-02-08 17:42:35 +0700

    fixed the issue

--- src/app/modules/parameters/pages/indicators/indicators.component.ts
+++ src/app/modules/parameters/pages/indicators/indicators.component.ts
@@ -3,7 +3,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { Indicator } from 'src/app/models/classes/indicator.model';
 import { IndicatorService } from 'src/app/services/indicator.service';
 import { IndicatorModalComponent } from './components/indicator-modal/indicator-modal.component';
+import { ThemeService } from 'src/app/services/theme.service';
 import { Theme } from 'src/app/models/classes/theme.model';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-indicators',
@@ -13,20 +15,34 @@ import { Theme } from 'src/app/models/classes/theme.model';
 export class IndicatorsComponent implements OnInit {
 
   indicators: Indicator[];
+  themes: Theme[];
 
   constructor(
     private indicatorService: IndicatorService,
+    private themeService :ThemeService,
+    private translateService: TranslateService,
     private dialog: MatDialog
   ) { }
 
   ngOnInit(): void {
     this.getIndicators();
+    this.getThemes();
+  }
+
+  get currentLang() {
+    return this.translateService.currentLang ? this.translateService.currentLang : this.translateService.defaultLang;
+  }
+
+  private getThemes(){
+    this.themeService.list().then((res: Theme[]) => {
+      this.themes = res.sort((a,b) => (a.name.en > b.name.en)? 1: -1);
+    });
   }
 
   private getIndicators() {
     this.indicatorService.list().then((res: Indicator[]) => {
-      this.indicators = res.sort((a,b) => (a.name.en > b.name.en)? 1: -1);      
-    });
+      this.indicators = res.sort((a,b) => (a.name.en > b.name.en)? 1: -1);
+    });    
   }
 
   onDelete(id: string) {
