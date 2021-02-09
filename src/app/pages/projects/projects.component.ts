9f6fb7b7af3abd73f169000bd98d308afd0f3dd9
Julien Foschia <julien.reliefapps@gmail.com>
2021-02-09 12:39:16 +0100

    adding the tooltip to the list of selected countries

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -46,6 +46,10 @@ export class ProjectsComponent implements OnInit, AfterViewChecked {
     return this.translateService.currentLang ? this.translateService.currentLang : this.translateService.defaultLang;
   }
 
+  get selectedCountries(): [] {
+    return this.filtersForm.value.countries as [];
+  }
+
   constructor(
     private fb: FormBuilder,
     private projectService: ProjectService,
