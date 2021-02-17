7c77ac0416445054c9a8db28071ff3aebccbab3f
Julien Foschia <julien.reliefapps@gmail.com>
2021-02-17 13:29:13 +0100

    Description of the forceTranslateService added

--- src/app/services/forcetranslate.service.ts
+++ src/app/services/forcetranslate.service.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
+
+// This service is used when we need to have a translation in a language different than the current one
 export class ForceTranslateService {
 
     constructor(private http: HttpClient) { }
