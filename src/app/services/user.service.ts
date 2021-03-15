713cd2b53ad9972ee8c131ad4a21ec7e50487f8c
Julien Foschia <julien.reliefapps@gmail.com>
2021-03-15 18:15:41 +0100

    fix project filter

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -11,7 +11,8 @@ export class UserService {
 
   public async list(): Promise<User[]> {
     const response: any = await this.apiService.get('/resources/user');
-    return (response.map(x => new User(x)) as User[]).sort((x: User, y: User) => x.name.localeCompare(y.name));
+    return (response.map(x => new User(x)) as User[])
+    .sort((x: User, y: User) => x.name.toLocaleLowerCase().localeCompare(y.name.toLocaleLowerCase()));
   }
 
   public async save(user: User): Promise<void> {
