970c907a5942b81cfd356d9ff628530c4cbd1743
Julien Foschia <julien.reliefapps@gmail.com>
2021-04-28 15:43:13 +0200

    removing useless oninit call

--- src/app/modules/project/modules/structure/pages/data-sources/components/data-source/data-source.component.ts
+++ src/app/modules/project/modules/structure/pages/data-sources/components/data-source/data-source.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Form } from 'src/app/models/classes/form.model';
 import { Project } from 'src/app/models/classes/project.model';
