c0f4a69146260888cd4e489e45505adec907b7a2
Pedro Diogo Machado <pedro@reliefapplications.org>
2021-04-28 15:51:50 -0300

    show groups in the logical-frames and data-sources cards

--- src/app/modules/project/modules/structure/pages/data-sources/components/data-source/data-source.component.ts
+++ src/app/modules/project/modules/structure/pages/data-sources/components/data-source/data-source.component.ts
@@ -1,6 +1,8 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Entity } from 'src/app/models/classes/entity.model';
 import { Form } from 'src/app/models/classes/form.model';
+import { Group } from 'src/app/models/classes/group.model';
 import { Project } from 'src/app/models/classes/project.model';
 import { environment } from 'src/environments/environment';
 
@@ -15,8 +17,50 @@ export class DataSourceComponent {
   @Input() project: Project;
   @Output() edit = new EventEmitter();
   @Output() delete = new EventEmitter();
+  public allOption: Group = new Group({id:'all', name: 'All'})
+  groups: Group[];
+  entities: Entity[];
+
+  private getGroupsSelected(){ 
+    if (this.form === null || this.form.entities === null){
+      return [];
+    }
+    
+    // if all entities are selected, just return the allOption
+    if (this.form.entities.length === this.project.entities.length){
+      return [this.allOption];
+    }
+  
+    // get the groups that have all members selected
+    let groups = this.project.groups.filter(g => {
+      for(let member of g.members){
+        if (!this.form.entities.includes(member) ){
+          return false;
+        }
+      }
+      return true;
+    })
+
+    return groups;
+  }
+
+  private filterEntities(): Entity[] {
+    let entities = [...this.form.entities];
+
+    for (let group of this.groups){
+      entities = entities.filter( e => !group.members.includes(e) );
+    }
+    
+    return entities;
+  }
 
   constructor(private translateService: TranslateService) { }
+
+  ngOnInit(): void {
+    this.allOption.members = this.form.entities;
+    this.groups = this.getGroupsSelected();
+    this.entities = this.filterEntities()
+  }
 
   get currentLang() {
     return this.translateService.currentLang ? this.translateService.currentLang : this.translateService.defaultLang;
--- src/app/modules/project/modules/structure/pages/logical-frames/components/logical-frame/logical-frame.component.ts
+++ src/app/modules/project/modules/structure/pages/logical-frames/components/logical-frame/logical-frame.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+import { Entity } from 'src/app/models/classes/entity.model';
+import { Group } from 'src/app/models/classes/group.model';
 import { LogicalFrame } from 'src/app/models/classes/logical-frame.model';
 import { Project } from 'src/app/models/classes/project.model';
 import { environment } from 'src/environments/environment';
@@ -16,6 +18,10 @@ export class LogicalFrameComponent implements OnInit {
   @Output() clone = new EventEmitter();
   @Output() edit = new EventEmitter();
   @Output() delete = new EventEmitter();
+  public allOption: Group = new Group({id:'all', name: 'All'})
+  groups: Group[];
+  entities: Entity[];
+
 
   get currentLang() {
     return this.translateService.currentLang ? this.translateService.currentLang : this.translateService.defaultLang;
@@ -29,9 +35,47 @@ export class LogicalFrameComponent implements OnInit {
     return `${environment.API_URL}/resources/project/${this.project.id}/logical-frame/${this.logicalFrame.id}.pdf?orientation=portrait&language=${this.currentLang}`;
   }
 
+  private getGroupsSelected(){ 
+    if (this.logicalFrame === null || this.logicalFrame.entities === null){
+      return [];
+    }
+    
+    // if all entities are selected, just return the allOption
+    if (this.logicalFrame.entities.length === this.project.entities.length){
+      return [this.allOption];
+    }
+  
+    // get the groups that have all members selected
+    let groups = this.project.groups.filter(g => {
+      for(let member of g.members){
+        if (!this.logicalFrame.entities.includes(member) ){
+          return false;
+        }
+      }
+      return true;
+    })
+
+    return groups;
+  }
+
+  private filterEntities(): Entity[] {
+    let entities = [...this.logicalFrame.entities];
+
+    for (let group of this.groups){
+      entities = entities.filter( e => !group.members.includes(e) );
+    }
+    
+    return entities;
+  }
+
   constructor(private translateService: TranslateService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.allOption.members = this.logicalFrame.entities;
+    this.groups = this.getGroupsSelected();
+    this.entities = this.filterEntities()
+  }
+  
 
   onClone() {
     this.clone.emit(this.logicalFrame);
