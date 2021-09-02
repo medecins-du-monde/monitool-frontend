01ac6345ef62d44fe06d6da1bcd73604cb5d73d9
Pedro Diogo Machado <pedro@reliefapplications.org>
2021-09-02 17:40:17 -0300

    linting error console.log in dev

--- src/app/pages/projects/projects.component.ts
+++ src/app/pages/projects/projects.component.ts
@@ -107,7 +107,7 @@ export class ProjectsComponent implements OnInit, OnDestroy, AfterViewChecked {
 
   ngOnInit(): void {
 
-    console.log("test - remove this console.log")
+    console.log('test - remove this console.log');
     this.filtersForm = this.fb.group({
       search: '',
       countries: [['0']],
