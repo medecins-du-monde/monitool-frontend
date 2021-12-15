e0c02addd0ac98b4ccce45afe724d5e084119465
helene <hpicard2021@gmail.com>
2021-12-15 10:44:48 +0100

    Remove functionalities from 244 from dev

--- src/app/modules/project/modules/input/pages/edit/edit.component.ts
+++ src/app/modules/project/modules/input/pages/edit/edit.component.ts
@@ -142,6 +142,7 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
   ngOnInit(): void {
 
     this.userService.showingInputModal.subscribe(val => {
+      console.log('val', val);
       this.showModal = val;
     });
     // Set the page with the normal size
@@ -225,11 +226,10 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
 
       // Subscribe to any changes in the form
         this.inputForm.valueChanges.subscribe(val => {
-          console.log('form', this.inputForm.value);
         // Convert the string input to a number
-        // this.convertToNumber(val);
+        this.convertToNumber(val);
         // Update of the total cell of each row or column
-          this.updateTotals(val);
+        this.updateTotals(val);
       });
 
       // Update the breadcrumbs informations
@@ -270,7 +270,7 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
         );
       }else{
         valuesGroup[e.id] = this.fb.array(
-          Array.from({length: this.countInputCells(e)}, () => null)
+          Array.from({length: this.countInputCells(e)}, () => 0)
         );
       }
     }
@@ -303,23 +303,19 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
       const table = this.tables[i];
       let x: number;
       let y: number;
-      let total = null;
+      let total = 0;
 
       // Update of the total for all rows
       // chose a row
       for (x = table.cols.length; x < (table.numberRows - 1); x += 1){
         // sum of the row
-        let sum = null;
+        let sum = 0;
         // iterate over all collumns for the row chosen
         for (y = 0; y < table.numberCols; y += 1){
           const inputPos = this.isInputCell(i, x, y);
           if (inputPos !== null){
             if (!isNaN(val.values[table.id][inputPos])){
-              if (val.values[table.id][inputPos]) {
-                sum += val.values[table.id][inputPos];
-              } else {
-                sum = null;
-              }
+              sum += val.values[table.id][inputPos];
             }
           }
         }
@@ -338,16 +334,12 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
       // Re-initialisation of the total after having used it for the columns
       total = 0;
       for (y = table.rows.length; y < (table.numberCols - 1); y += 1){
-        let sum = null;
+        let sum = 0;
         for (x = 0; x < table.numberRows; x += 1){
           const inputPos = this.isInputCell(i, x, y);
           if (inputPos !== null){
             if (!isNaN(val.values[table.id][inputPos])){
-              if (val.values[table.id][inputPos]) {
-                sum += val.values[table.id][inputPos];
-              } else {
-                sum = null;
-              }
+              sum += +val.values[table.id][inputPos];
             }
           }
         }
@@ -356,10 +348,8 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
         total += sum;
       }
       // if the table has multiple rows and collums the total in the last cell needs to be updated
-      if (total && total !== 0){
+      if (total !== 0){
         table.value[table.numberRows - 1][table.numberCols - 1] = total;
-      } else {
-        table.value[table.numberRows - 1][table.numberCols - 1] = null;
       }
     }
   }
@@ -469,7 +459,6 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
         },
         // updates the inputForm everytime we change something in the table
         beforeChange: (core, changes) => {
-          console.log('changes 1', this.inputForm.value);
           if (changes !== null){
             for (let i = 0; i < changes.length; i += 1){
               const change = changes[i];
@@ -489,7 +478,6 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
                 if (pos !== null && typeof newValue === 'number'){
                   change[3] = newValue;
                   this.inputForm.get('values').get(element.id).get(`${pos}`).setValue(newValue);
-                  console.log('changes', this.inputForm.value);
                 }
               }else{
                 // dont apply this change
@@ -685,7 +673,6 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
     if (this.previousInput){
       for (const e of this.form.elements){
         if (this.previousInput && this.previousInput.values && this.previousInput.values[e.id]){
-          console.log('1', this.previousInput.values[e.id]);
           this.inputForm.get('values').get(e.id).setValue(this.previousInput.values[e.id]);
         }
       }
@@ -693,38 +680,6 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
       this.updateTotals(this.inputForm.value);
     }
 
-  }
-
-  fillWithZero(): void {
-        // Get all the values already in the current form
-        const valuesGroup = {};
-        for (const e of this.form.elements){
-          if (this.input && this.input.values && this.input.values[e.id]){
-            valuesGroup[e.id] = this.fb.array(
-              this.input.values[e.id]
-            );
-          }else{
-            valuesGroup[e.id] = this.fb.array(
-              Array.from({length: this.countInputCells(e)}, () => 0)
-            );
-          }
-        }
-
-        // Create the formGroup
-        this.inputForm = this.fb.group({
-          _id: (this.input && this.input.id) ? this.input.id : `input:${this.project.id}:${this.form.id}:${this.site.id}:${this.timeSlotDate}`,
-          entity: this.site.id,
-          form: this.form.id,
-          period: this.timeSlotDate,
-          project: this.project.id,
-          rev: (this.input && this.input.rev) ? this.input.rev : null,
-          values: this.fb.group(valuesGroup)
-        });
-
-        // Fill the init value with the current value in order to be able to reset
-        this.initValue = _.cloneDeep(this.inputForm) as FormGroup;
-        this.createTable();
-        this.updateTotals(this.inputForm.value);
   }
 
   // Save the current input and redirect the user to the input home page
@@ -781,13 +736,12 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate{
     this.createTable();
     this.updateTotals(this.inputForm.value);
     this.inputForm.valueChanges.subscribe(val => {
-      console.log('val', val);
       this.convertToNumber(val);
       this.updateTotals(val);
     });
   }
 
-  ngOnDestroy(): void{
+  ngOnDestroy(){
     this.subscription.unsubscribe();
   }
 }
