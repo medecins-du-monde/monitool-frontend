7b799ce8971f90c3aca9c76bba5b602b09ab5aba
helene <hpicard2021@gmail.com>
2022-04-11 13:11:58 +0200

    refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -187,7 +187,7 @@ export class AppComponent implements OnInit, AfterViewChecked {
 
     /*
 
-    // check serviceworker for updates, disabled for now
+    // check serviceworker for updates
     if (this.swUpdate.isEnabled) {
       interval(60000).subscribe(() => this.swUpdate.checkForUpdate().then(() => {
         // checking for updates
