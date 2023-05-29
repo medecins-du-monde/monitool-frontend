61c97b2dc0f0ce7fd7206277ba24b43cbe1d2e29
matheus-relief <matheus.reliefapps@gmail.com>
2023-05-29 13:47:04 -0300

    feat: users archiving and filters

--- src/app/modules/parameters/pages/users/components/user/user.component.ts
+++ src/app/modules/parameters/pages/users/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { User } from 'src/app/models/classes/user.model';
 import { UserModalComponent } from '../user-modal/user-modal.component';
@@ -8,18 +8,16 @@ import { UserModalComponent } from '../user-modal/user-modal.component';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
-
+export class UserComponent {
   @Input() user: User;
 
   @Output() edit = new EventEmitter();
+  @Output() delete = new EventEmitter<User>();
+  @Output() restore = new EventEmitter<User>();
 
-  constructor(private dialog: MatDialog) {
-  }
+  constructor(private dialog: MatDialog) {}
 
-  ngOnInit(): void {}
-
-  openDialog() {
+  openDialog(): void {
     const dialogRef = this.dialog.open(UserModalComponent, { data: this.user });
 
     dialogRef.afterClosed().subscribe(res => {
@@ -29,4 +27,13 @@ export class UserComponent implements OnInit {
     });
   }
 
+  onDelete(): void {
+    this.user.active = false;
+    this.delete.emit(this.user);
+  }
+
+  onRestore(): void {
+    this.user.active = true;
+    this.restore.emit(this.user);
+  }
 }
