d0fa432e5f3ab1451c42ae8694dedb9a5cdcfe63
Maximo Monroy <maximo.reliefapps@gmail.com>
2023-07-17 11:33:49 +0200

    uat test

--- src/app/components/report/reporting-table/reporting-table.component.ts
+++ src/app/components/report/reporting-table/reporting-table.component.ts
@@ -67,7 +67,7 @@ type RowCommentInfo = {
   selector: 'app-reporting-table',
   templateUrl: './reporting-table.component.html',
   styleUrls: ['./reporting-table.component.scss'],
-  // changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ReportingTableComponent
   implements OnInit, OnDestroy, AfterViewInit {
@@ -375,7 +375,6 @@ export class ReportingTableComponent
         if (JSON.stringify(this.content) !== JSON.stringify(newContent)) {
           this.content = newContent;
           this.updateTableContent();
-          // this.changeDetectorRef.detectChanges();
         }
       })
     );
