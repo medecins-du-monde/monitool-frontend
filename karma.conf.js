8f13c4d4fd13f0cd84db53e507333331556e73f5
Maximo Monroy <maximo.reliefapps@gmail.com>
2023-12-01 15:56:22 +0100

    update karma conf

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,10 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-process.env.CHROME_BIN = require('puppeteer').executablePath()
-
 module.exports = function (config) {
+
+  const process = require('process');
+  process.env.CHROME_BIN = require('puppeteer').executablePath();
+
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
