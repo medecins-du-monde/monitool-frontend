75f0ea0bf3952eacb1ca96ef6d098902b3da8ee1
Maximo Monroy <maximo.reliefapps@gmail.com>
2024-05-14 08:54:07 +0200

    Update rights table

--- src/app/mocked/hint-user-element.mocked.ts
+++ src/app/mocked/hint-user-element.mocked.ts
@@ -10,6 +10,8 @@ export const HintUserData: HintUserElement[] = [
     {untitled: 'TooltipRoleHint.EditProjectsStructure', admin: true, project: true, common: true},
     {untitled: 'TooltipRoleHint.SeeAllProjectsReporting', admin: true, project: true, common: true},
     {untitled: 'TooltipRoleHint.CreateProjects', admin: true, project: true, common: false},
+    {untitled: 'TooltipRoleHint.CloneProjects', admin: true, project: true, common: false},
+    {untitled: 'TooltipRoleHint.CloneProjectsData', admin: true, project: true, common: false},
     {untitled: 'TooltipRoleHint.CreateAndEditThematics', admin: true, project: false, common: false},
     {untitled: 'TooltipRoleHint.CreateAndEditCrossCutting', admin: true, project: false, common: false},
     {untitled: 'TooltipRoleHint.EditOtherUsersRoles', admin: true, project: false, common: false},
