8422cb5da658508e999f9eb2431cf0e18ce90a30
Maximo Monroy <maximo.reliefapps@gmail.com>
2024-06-17 11:31:07 +0200

    Fix issue with numbers & null formulas

--- src/app/modules/parameters/pages/indicators/components/indicator-modal/indicator-modal.component.ts
+++ src/app/modules/parameters/pages/indicators/components/indicator-modal/indicator-modal.component.ts
@@ -140,6 +140,7 @@ export class IndicatorModalComponent implements OnInit, OnDestroy {
   onSubmit() {
     const computation = this.indicatorForm.controls.computation as FormGroup;
     computation.controls.formula.enable();
+    computation.controls.formula.patchValue(String(computation.value.formula));
     const indicator = new Indicator(this.indicatorForm.value);
     // Show warning for indicators using prev formula
     if (
