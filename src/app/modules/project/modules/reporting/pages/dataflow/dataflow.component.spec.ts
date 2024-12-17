fab7259db29db25bb8a8d42b79d49ed7ddef9ef7
Maximo Monroy <maximo.reliefapps@gmail.com>
2024-12-17 08:26:29 +0100

    lint fix

--- src/app/modules/project/modules/reporting/pages/dataflow/dataflow.component.spec.ts
+++ src/app/modules/project/modules/reporting/pages/dataflow/dataflow.component.spec.ts
@@ -1,10 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { DataflowComponent } from './dataflow.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('DataflowComponent', () => {
   let component: DataflowComponent;
   let fixture: ComponentFixture<DataflowComponent>;
+  
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [DataflowComponent],
+      imports: [HttpClientTestingModule, RouterTestingModule]
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     TestBed.configureTestingModule({
