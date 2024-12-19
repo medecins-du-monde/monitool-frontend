38902b4458b3253159edfe08fcbb46be0a265867
Maximo Monroy <maximo.reliefapps@gmail.com>
2024-12-19 15:47:46 +0100

    remove log

--- src/app/modules/project/modules/input/pages/edit/edit.component.ts
+++ src/app/modules/project/modules/input/pages/edit/edit.component.ts
@@ -590,7 +590,6 @@ export class EditComponent implements OnInit, OnDestroy, ComponentCanDeactivate 
 
               let newValue;
               if (/^\s*([-+]?)((\d+(\.\d*)?)|(\.\d+))(?:\s*([-+*/])\s*((?:\s[-+])?((\d+(\.\d*)?)|(\.\d+)))\s*)+$/.test(change[3])) {
-                console.log(change[3]);
                 newValue = eval(change[3]);
               } else {
                 newValue = change[3];
