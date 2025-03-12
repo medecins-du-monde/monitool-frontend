07008b392ba4f61545ad06ddc3ae170dd4aa12c1
Maximo Monroy <maximo.reliefapps@gmail.com>
2025-03-12 15:34:04 +0100

    Improved solution

--- src/app/modules/project/modules/structure/pages/data-sources/components/form-element-edit/form-element-edit.component.ts
+++ src/app/modules/project/modules/structure/pages/data-sources/components/form-element-edit/form-element-edit.component.ts
@@ -45,7 +45,6 @@ export class FormElementEditComponent implements OnInit {
 
   receiveStructure(event) {
     this.chosenStructure = event;
-    this.elementForm.controls.distribution.patchValue(this.chosenStructure);
   }
 
   get partitions(): UntypedFormArray {
