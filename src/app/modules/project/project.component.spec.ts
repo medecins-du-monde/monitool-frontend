801ffe295c1ea440f779cff57f0c891d67c2f88b
Maximo Monroy <maximo.reliefapps@gmail.com>
2025-03-26 10:18:28 +0100

    New informations for country sistem

--- src/app/modules/project/project.component.spec.ts
+++ src/app/modules/project/project.component.spec.ts
@@ -3,6 +3,7 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { ProjectComponent } from './project.component';
+import { MatLegacyDialog as MatDialogModule } from '@angular/material/legacy-dialog';
 
 describe('ProjectComponent', () => {
   let component: ProjectComponent;
@@ -11,7 +12,7 @@ describe('ProjectComponent', () => {
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ProjectComponent],
-      imports: [RouterTestingModule, HttpClientTestingModule]
+      imports: [RouterTestingModule, HttpClientTestingModule, MatDialogModule]
     }).compileComponents();
   }));
 
