4a671966d74c9afaa4106832482ce58612339632
Maximo Monroy <maximo.reliefapps@gmail.com>
2025-08-25 11:30:47 +0200

    test fixes

--- src/app/modules/project/modules/input/components/import-modal/import-modal.component.spec.ts
+++ src/app/modules/project/modules/input/components/import-modal/import-modal.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImportModalComponent } from './import-modal.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
+
 
 describe('ImportModalComponent', () => {
   let component: ImportModalComponent;
@@ -8,7 +11,12 @@ describe('ImportModalComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ImportModalComponent]
+      imports: [HttpClientTestingModule],
+      declarations: [ImportModalComponent],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] }
+      ]
     });
     fixture = TestBed.createComponent(ImportModalComponent);
     component = fixture.componentInstance;
--- src/app/services/data-import.service.spec.ts
+++ src/app/services/data-import.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataImportService } from './data-import.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('DataImportService', () => {
   let service: DataImportService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+      ]
+    });
     service = TestBed.inject(DataImportService);
   });
 
