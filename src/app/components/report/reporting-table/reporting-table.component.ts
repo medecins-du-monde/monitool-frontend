0648e401b9e4f40fe4304f11bda73a4c5a163427
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2025-10-06 12:48:56 +0200

    New graph colors from MdM style guidelines

--- src/app/components/report/reporting-table/reporting-table.component.ts
+++ src/app/components/report/reporting-table/reporting-table.component.ts
@@ -199,16 +199,12 @@ export class ReportingTableComponent
   dataSource = new MatTableDataSource([]);
 
   COLORS = [
-    '#1f77b4',
-    '#ff7f0e',
-    '#2ca02c',
-    '#d62728',
-    '#9467bd',
-    '#8c564b',
-    '#e377c2',
-    '#7f7f7f',
-    '#bcbd22',
-    '#17becf'
+    '#005da8',
+    '#e84f1c',
+    '#00b09b',
+    '#bcdfeb',
+    '#1d1a54',
+    '#ffdd00',
   ];
 
   currentColorIndex = 0;
