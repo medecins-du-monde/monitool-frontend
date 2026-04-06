0d3226defa9d589a933c55fe4d02bba588eb91ad
Maximo-reliefapps <maximo.reliefapps@gmail.com>
2026-04-06 11:22:32 +0200

    ADd dashboard comments, reorder and other improvs

---
+++ src/app/modules/project/modules/reporting/pages/dashboard/comment-modal/comment-modal.component.spec.ts
@@ -0,0 +1,34 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { MatLegacyDialogModule as MatDialogModule, MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
+import { TranslateModule } from '@ngx-translate/core';
+
+import { CommentModalComponent } from './comment-modal.component';
+
+describe('CommentModalComponent', () => {
+  let component: CommentModalComponent;
+  let fixture: ComponentFixture<CommentModalComponent>;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        MatDialogModule,
+        TranslateModule.forRoot(),
+      ],
+      declarations: [CommentModalComponent, ],
+      providers: [
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: [] }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CommentModalComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
